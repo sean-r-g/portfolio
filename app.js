7888fe8f5f209236a382f6c9f76765fa6538b54f
Sean Gillis <seangillis@Seans-MBP.attlocal.net>
2022-04-26 14:17:01 -0500

    Overall structure and page linking complete

---
+++ app.js
@@ -0,0 +1,31 @@
+$(() => {
+  let currentImgIndex = 0
+  let numOfImages = $('.carousel-images').children().length - 1
+
+  $('.next').on('click', () => {
+    $('.carousel-images').children().eq(currentImgIndex).css('display', 'none')
+    if (currentImgIndex < numOfImages){
+      currentImgIndex++
+    } else {
+      currentImgIndex = 0
+    }
+    $('.carousel-images').children().eq(currentImgIndex).css('display', 'block')
+  })
+
+  $('.previous').on('click', () => {
+    $('.carousel-images').children().eq(currentImgIndex).css('display', 'none')
+    if(currentImgIndex > 0) {
+      currentImgIndex--
+    } else {
+      currentImgIndex = numOfImages
+    }
+    $('.carousel-images').children().eq(currentImgIndex).css('display', 'block')
+  })
+
+
+
+
+
+
+
+});
