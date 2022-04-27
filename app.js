e269d213694d28704b3943b5d0c2987e659874c9
Sean Gillis <seangillis@Seans-MBP.attlocal.net>
2022-04-27 09:26:29 -0500

    main page slides functioning

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 $(() => {
   let currentImgIndex = 0
-  let numOfImages = $('.carousel-images').children().length - 1
+  let numOfImages = $('.project-screenshots').children().length - 1
 
   $('.next').on('click', () => {
-    $('.carousel-images').children().eq(currentImgIndex).css('display', 'none')
+    $('.project-screenshots').children().eq(currentImgIndex).css('display', 'none')
     if (currentImgIndex < numOfImages){
       currentImgIndex++
     } else {
       currentImgIndex = 0
     }
-    $('.carousel-images').children().eq(currentImgIndex).css('display', 'block')
+    $('.project-screenshots').children().eq(currentImgIndex).css('display', 'block')
   })
 
   $('.previous').on('click', () => {
-    $('.carousel-images').children().eq(currentImgIndex).css('display', 'none')
+    $('.project-screenshots').children().eq(currentImgIndex).css('display', 'none')
     if(currentImgIndex > 0) {
       currentImgIndex--
     } else {
       currentImgIndex = numOfImages
     }
-    $('.carousel-images').children().eq(currentImgIndex).css('display', 'block')
+    $('.project-screenshots').children().eq(currentImgIndex).css('display', 'block')
   })
 
 
