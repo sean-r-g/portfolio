6562a930999d6bd2ffeccd1a7782e3b64f011051
Sean Gillis <seangillis@Seans-MBP.attlocal.net>
2022-04-27 15:51:40 -0500

    adding more jQuery - time to commit

--- app.js
+++ app.js
@@ -22,6 +22,23 @@ $(() => {
     $('.project-screenshots').children().eq(currentImgIndex).css('display', 'block')
   })
 
+  ////Show social name
+  $('#github').css('color', 'transparent')
+  $('#linkedin').css('color', 'transparent')
 
+  $('#li-logo').on('mouseover', () =>[
+    $('#linkedin').css('color', 'white').css('background-color', '#0072b1').css('border-radius', '10px').css('text-shadow', '2px 2px 4px black')
+  ])
+  $('#gh-logo').on('mouseover', () =>[
+    $('#github').css('color', 'lightgray').css('background-color', 'black').css('border-radius', '10px').css('text-shadow', '2px 2px 4px black')
+  ])
+
+  $('#li-logo').on('mouseout', () =>[
+    $('#linkedin').css('color', 'transparent').css('background-color', 'transparent').css('text-shadow', 'none')
+  ])
+  $('#gh-logo').on('mouseout', () =>[
+    $('#github').css('color', 'transparent').css('background-color', 'transparent').css('text-shadow', 'none')
+  ])
+  
 });
 
